1f7e3f5fac7d3bf0b095670cbccdd65ed2cc3d21
Roman - <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>
2023-08-20 03:10:53 +0200

    Prev site done

--- script.js
+++ script.js
@@ -58,16 +58,111 @@ menuBar.addEventListener("click", (e) => {
 
 //Toggle tabs
 const tabs = document.querySelectorAll(".tabs");
-btns.forEach(button => {
+btns.forEach((button) => {
   button.addEventListener("click", () => {
-    const btn = button.textContent.replace(/\s+/g, '');
-    tabs.forEach(tab => {
+    const btn = button.textContent.replace(/\s+/g, "");
+    tabs.forEach((tab) => {
       tab.classList.add("hidden");
       tab.classList.remove("slideIn");
-      if(tab.dataset.tab === btn) {
+      if (tab.dataset.tab === btn) {
         tab.classList.remove("hidden");
         tab.classList.add("slideIn");
       }
-    })
+    });
   });
-})
+});
+
+//Animate video on hover
+const videoTiles = document.querySelectorAll(".videotile");
+for (let i = 1; i <= videoTiles.length; i++) {
+  const vidElement = document.querySelector(`.vid${i}`);
+  const videoElement = document.querySelector(`.video${i}`);
+
+  vidElement.addEventListener("mouseenter", () => {
+    videoElement.play();
+  });
+
+  vidElement.addEventListener("mouseleave", () => {
+    videoElement.pause();
+  });
+}
+
+//Menu hover liught animation
+const handleOnMouseMove2 = (e) => {
+  const { currentTarget: target } = e;
+  const rect = target.getBoundingClientRect(),
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
+  target.style.setProperty("--mouse-x2", `${x}px`);
+  target.style.setProperty("--mouse-y2", `${y}px`);
+};
+
+for (const card of document.querySelectorAll(".videotile")) {
+  card.onmousemove = (e) => handleOnMouseMove2(e);
+}
+
+
+
+
+//Handle video preview
+const videotilescontainer = document.querySelector(".videotilescontainer");
+const prevVideo = document.querySelector(".prev-vid");
+const prevTitle = document.querySelector(".prev-title");
+const prevDesc = document.querySelector(".prev-desc");
+const overlay = document.querySelector(".overlay");
+
+videotilescontainer.addEventListener("click", (e) => {
+  const len = +e.target.children.length;
+  if(len === 1) {
+    const title = e.target.parentNode.children[1].textContent;
+    prevTitle.innerHTML = title;
+    const desc = e.target.parentNode.children[2].textContent;
+    prevDesc.innerHTML = desc + "<br><br>" + desc + "<br><br>" + desc + "<br><br>" + desc;
+  } 
+  if(len === 3) {
+    const title = e.target.children[1].textContent;
+    prevTitle.innerHTML = title;
+    const desc = e.target.children[2].textContent;
+    prevDesc.innerHTML = desc + "<br><br>" + desc + "<br><br>" + desc + "<br><br>" + desc;
+  }
+
+  prevVideo.play();
+  overlay.classList.remove("hidden");
+});
+
+
+//Close preview
+const closePrevModalBtn = document.querySelector(".close-prevmodal");
+closePrevModalBtn.addEventListener("click", () => {
+  overlay.classList.add("hidden");
+  prevTitle.innerHTML = '';
+  prevDesc.innerHTML = '';
+});
+
+//Toggle fullscreen vid
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+      // If the document is not currently in fullscreen mode
+      if (prevVideo.requestFullscreen) {
+          prevVideo.requestFullscreen();
+      } else if (prevVideo.mozRequestFullScreen) { // Firefox
+          prevVideo.mozRequestFullScreen();
+      } else if (prevVideo.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+          prevVideo.webkitRequestFullscreen();
+      } else if (prevVideo.msRequestFullscreen) { // IE/Edge
+          prevVideo.msRequestFullscreen();
+      }
+  } else {
+      // If the document is currently in fullscreen mode, exit fullscreen
+      if (document.exitFullscreen) {
+          document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) { // Firefox
+          document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) { // Chrome, Safari, and Opera
+          document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) { // IE/Edge
+          document.msExitFullscreen();
+      }
+  }
+}
+prevVideo.addEventListener("click", toggleFullscreen);
